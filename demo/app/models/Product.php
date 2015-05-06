f16b2058ef5455af86317460a6cbbd78a6d12700
vagrant <vagrant@l-dev>
2015-05-06 16:52:16 +0000

    add project demo

---
+++ demo/app/models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+class Product extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $price;
+
+    /**
+     *
+     * @var string
+     */
+    public $description;
+
+}
